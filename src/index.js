5999cc04d6d3044ae6e21f8a194fdba70743f785
Mark-Pro <670188307@qq.com>
2018-10-09 14:40:24 +0800

    改为jquery 调用

--- src/index.js
+++ src/index.js
@@ -1,29 +1,14 @@
 import _ from 'lodash';
 import './style.css';
 import icon from './icon.jpg';
+import $ from 'jquery';
 
-function component() {
-    var element = document.createElement("div");
-
-    element.innerHTML = _.join(['hello','webpack'], " ");
-    //添加类名
-    element.classList.add('hello');
-    //添加图片
-    var myIcon = new Image();
-    myIcon.src = icon;
-    element.appendChild(myIcon);
-    //返回插入
-    return element;
-}
-document.body.appendChild( component() )
+console.log( icon )
 
 
+$("body").append($("<div class='hello'> <div/>"));
 
+var img = $("<img>");
+img.attr("src", icon);
 
-
-
-
-
-
-
-
+$(".hello").html(_.join(['hello', 'webpack'], " ")).append(img);
