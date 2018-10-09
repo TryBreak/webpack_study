7c43e9cf630a24f77d15aa269300edfcaf8142ae
Mark-Pro <670188307@qq.com>
2018-10-09 14:19:57 +0800

    add

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
-import './style.css'
+import './style.css';
+import icon from './icon.jpg';
+
 function component() {
     var element = document.createElement("div");
 
     element.innerHTML = _.join(['hello','webpack'], " ");
+    //添加类名
     element.classList.add('hello');
-    
+    //添加图片
+    var myIcon = new Image();
+    myIcon.src = icon;
+    element.appendChild(myIcon);
+    //返回插入
     return element;
 }
 document.body.appendChild( component() )
