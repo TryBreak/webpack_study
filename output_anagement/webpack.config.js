2934e9278333b36cfd5b1ab9413d0a70d071c60e
Mark-Pro <670188307@qq.com>
2018-10-10 10:50:15 +0800

    add

--- output_anagement/webpack.config.js
+++ output_anagement/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
     mode: 'development',
     
@@ -9,7 +9,10 @@ module.exports = {
         print: './src/print.js'
     },
     
+    devtool: 'inline-source-map',
+
     plugins:[
+        new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             title: 'Output Management',
         })
