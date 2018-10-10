d8fcec73509d6334676a25e063b884447a2f6e7f
Mark-Pro <670188307@qq.com>
2018-10-10 16:53:18 +0800

    add

--- HMR/src/index.js
+++ HMR/src/index.js
@@ -3,11 +3,27 @@ import $ from 'jquery';
 import printMe from './print.js';
 
 $("body").append($("<div class='hello'> </div>"));
-$(".hello").html( _.join(['hello', 'webpack'], " ") );
+$(".hello").html(_.join(['hello', 'webpack'], " "));
 
 $("body").append($("<button class='btn'>Click me and check the console!</button>"));
 
 $('.btn').click(printMe);
+
+
+if (module.hot) {
+    module.hot.accept('./print.js', function () {
+        console.log('Accepting the updated printMe module!');
+        printMe();
+    })
+}
+
+
+
+
+
+
+
+
 
 
 
